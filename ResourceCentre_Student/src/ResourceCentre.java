8228cae570635fb6f25b33f28c615029f41a59c1
21038896 <21038896@myrp.edu.sg>
2022-07-22 09:50:28 +0800

    Retrieve methods refactor

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -132,10 +132,16 @@ public class ResourceCentre {
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", camcorderList.get(i).getAssetTag(),
-					camcorderList.get(i).getDescription(),
-					ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable()),
-					camcorderList.get(i).getDueDate(), camcorderList.get(i).getOpticalZoom());
+			String assetTag = camcorderList.get(i).getAssetTag();
+			String description = camcorderList.get(i).getDescription();
+			boolean isAvailable = camcorderList.get(i).getIsAvailable();
+			String dueDate = camcorderList.get(i).getDueDate();
+			int opticalZoom = camcorderList.get(i).getOpticalZoom();
+			
+			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", assetTag,
+					description,
+					ResourceCentre.showAvailability(isAvailable),
+					dueDate, opticalZoom);
 		}
 		return output;
 	}
@@ -153,10 +159,14 @@ public class ResourceCentre {
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(),
-					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(), chromebookList.get(i).getDueDate());
+			String assetTag = chromebookList.get(i).getAssetTag();
+			String description = chromebookList.get(i).getDescription();
+			boolean isAvailable = chromebookList.get(i).getIsAvailable();
+			String dueDate = chromebookList.get(i).getDueDate();
+			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", assetTag,
+					description,
+					ResourceCentre.showAvailability(isAvailable),
+					dueDate, dueDate);
 		}
 
 		return output;
