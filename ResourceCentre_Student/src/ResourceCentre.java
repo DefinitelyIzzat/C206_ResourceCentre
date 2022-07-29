ad32367d15b5fc7d16ff98c06f78581ed64bf0a5
21022805 <21022805@10.174.115.0>
2022-07-29 15:01:19 +0800

    pushed

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -20,7 +20,7 @@ public class ResourceCentre {
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-				// View all items
+				// View all items.
 				ResourceCentre.viewAllCamcorder(camcorderList);
 				ResourceCentre.viewAllChromebook(chromebookList);
 
